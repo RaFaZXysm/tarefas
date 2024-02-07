a23eec6a3aef22e3d47c50e45d8ca53735815943
RAFAEL ALVES <124845062+RaFaZXysm@users.noreply.github.com>
2024-02-07 18:14:34 -0300

    Add files via upload

---
+++ Crie um script que receba 4 valores e retorne a média.py
@@ -0,0 +1,11 @@
+# Crie um script que receba 4 valores e retorne a média dos valores.
+# media = (valor1 + valor2 + valor3 + valor4)/4
+
+valor1 = float(input('Digite o 1º valor: '))
+valor2 = float(input('Digite o 2º valor: '))
+valor3 = float(input('Digite o 3º valor: '))
+valor4 = float(input('Digite o 4º valor: '))
+
+media = (valor1 + valor2 + valor3 + valor4) 
+
+print ('A média geral dos valores é: ',media)
